e29140295efb61b33aef35fead8221547cbaf4a3
NTMT <77571605+NTMTriett@users.noreply.github.com>
2023-11-18 18:00:50 +0700

    Delete server/src/planets.mongo.js
    sameone

--- server/src/planets.mongo.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const planetSchema=new mongoose.Schema({
-    keplerName:{
-        type:String,
-        require:true,
-    }
-});
-
-module.exports = mongoose.model('Planet',planetSchema);
